0cec656909cd8b0f2f9d6404b7c549e9a2a67ebe
Marimg0528 <160878613+marimg0528@users.noreply.github.com>
2024-03-07 10:58:26 -0500

    Update s_t.py

--- s_t.py
+++ s_t.py
@@ -66,7 +66,7 @@ if result:
     text = str(result.get("GET_TEXT"))
     in_lang = st.selectbox(
         "Selecciona el lenguaje de Entrada",
-        ("Inglés", "Español", "Bengali", "Coreano", "Mandarín", "Japonés", "Francés"),
+        ("Inglés", "Español", "Bengali", "Coreano", "Mandarín", "Japonés", "Francés", "Alemán"),
     )
     if in_lang == "Inglés":
         input_language = "en"
@@ -82,10 +82,12 @@ if result:
         input_language = "ja"
     elif in_lang == "Francés":
         input_language = "fr"
+    elif in_lang == "Alemán":
+        input_language = "de"
     
     out_lang = st.selectbox(
         "Selecciona el lenguaje de salida",
-        ("Inglés", "Español", "Bengali", "Coreano", "Mandarín", "Japonés", "Francés"),
+        ("Inglés", "Español", "Bengali", "Coreano", "Mandarín", "Japonés", "Francés", "Alemán"),
     )
     if out_lang == "Inglés":
         output_language = "en"
@@ -101,6 +103,8 @@ if result:
         output_language = "ja"
     elif out_lang == "Francés":
         output_language = "fr"
+    elif out_lang == "Alemán":
+        output_language = "de"
     
     english_accent = st.selectbox(
         "Selecciona el acento",
