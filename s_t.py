21487a55bebf45e9587234496a6aca7e734a6e9d
Marimg0528 <160878613+marimg0528@users.noreply.github.com>
2024-03-07 10:24:56 -0500

    Update s_t.py

--- s_t.py
+++ s_t.py
@@ -66,7 +66,7 @@ if result:
     text = str(result.get("GET_TEXT"))
     in_lang = st.selectbox(
         "Selecciona el lenguaje de Entrada",
-        ("Inglés", "Español", "Bengali", "Coreano", "Mandarín", "Japonés"),
+        ("Inglés", "Español", "Bengali", "Coreano", "Mandarín", "Japonés", "Francés", "Alemán"),
     )
     if in_lang == "Inglés":
         input_language = "en"
@@ -80,10 +80,14 @@ if result:
         input_language = "zh-cn"
     elif in_lang == "Japonés":
         input_language = "ja"
+    elif in_lang == "Francés":
+        input_languaje = "fr"
+     elif in_lang == "Alemán":
+        input_languaje = "al"
     
     out_lang = st.selectbox(
         "Selecciona el lenguaje de salida",
-        ("Inglés", "Español", "Bengali", "Coreano", "Mandarín", "Japonés"),
+        ("Inglés", "Español", "Bengali", "Coreano", "Mandarín", "Japonés","Francés", "Alemán"),
     )
     if out_lang == "Inglés":
         output_language = "en"
@@ -97,6 +101,10 @@ if result:
         output_language = "zh-cn"
     elif out_lang == "Japonés":
         output_language = "ja"
+    elif in_lang == "Francés":
+        input_languaje = "fr"
+     elif in_lang == "Alemán":
+        input_languaje = "al"
     
     english_accent = st.selectbox(
         "Selecciona el acento",
